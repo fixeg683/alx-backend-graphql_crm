6daeb8ddb9b46ce469c85554502709c9e0b6cfad
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2026-01-05 02:56:01 -0500

    Rename and modify function to log CRM heartbeat

--- crm/cron.py
+++ crm/cron.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
-def update_low_stock():
+def log_crm_heartbeat():
+    timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+
     transport = RequestsHTTPTransport(
         url="http://localhost:8000/graphql",
         verify=True,
@@ -14,22 +16,17 @@ def update_low_stock():
         fetch_schema_from_transport=False
     )
 
-    mutation = gql("""
-    mutation {
-        updateLowStockProducts {
-            message
-            products {
-                name
-                stock
-            }
-        }
+    query = gql("""
+    query {
+        hello
     }
     """)
 
-    result = client.execute(mutation)
+    try:
+        client.execute(query)
+        status = "CRM is alive"
+    except Exception:
+        status = "CRM heartbeat failed"
 
-    with open("/tmp/low_stock_updates_log.txt", "a") as f:
-        for product in result["updateLowStockProducts"]["products"]:
-            f.write(
-                f"{datetime.now()} {product['name']} -> Stock: {product['stock']}\n"
-            )
+    with open("/tmp/crm_heartbeat_log.txt", "a") as f:
+        f.write(f"{timestamp} {status}\n")
